b64c926b7c7aa8c64dac1fd212ee4bcd12f208c7
陈武新 <785299033@qq.com>
2020-05-08 16:18:39 +0800

    更新资料

--- src/main/java/com/meipiao/StudyManualApplication.java
+++ src/main/java/com/meipiao/StudyManualApplication.java
@@ -1,9 +1,11 @@
 package com.meipiao;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.meipiao.dao")
 public class StudyManualApplication {
 
     public static void main(String[] args) {
