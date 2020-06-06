7ded4f272523a8ba9cc81ad073e25c5ab37da1da
陈武新 <785299033@qq.com>
2020-06-06 14:30:53 +0800

    传递依赖

--- spirngboot-study-login/src/main/java/com/meipiao/login/controller/LoginController.java
+++ spirngboot-study-login/src/main/java/com/meipiao/login/controller/LoginController.java
@@ -1,16 +1,11 @@
 package com.meipiao.login.controller;
 
-import com.meipiao.login.config.RedisUtil;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.context.request.WebRequest;
 
-import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
 import java.util.List;
 
